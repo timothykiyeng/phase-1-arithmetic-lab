f8f52cc4759348b2a8241335152c459420e8c4e1
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-02 14:04:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(9, 18, 16, 20);
